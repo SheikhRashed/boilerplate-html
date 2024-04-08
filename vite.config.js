958bfeb76671cc417f3a2dffcaa1bfec47897fb5
SheikhRashed <devrashed06@gmail.com>
2024-04-08 21:04:14 +0600

    update/vituum: added nunjucks

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
-import { resolve } from 'path';
+import vituum from 'vituum';
+import path, { resolve } from 'path';
 import { defineConfig } from 'vite';
+import Inspect from 'vite-plugin-inspect';
+import nunjucks from '@vituum/vite-plugin-nunjucks'
+import timeReporter from 'vite-plugin-time-reporter';
 
 export default defineConfig({
-  root: resolve(__dirname, 'src'),
-  build: {
-    outDir: resolve(__dirname, 'dist'),
-    assetsDir: 'assets',
-  },
+  plugins:[
+    vituum(),
+    Inspect(),
+    timeReporter(),
+    nunjucks({
+      root: resolve(__dirname, 'src'),
+    }),
+  ]
 });
